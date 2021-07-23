b6dab568abb54a6c3a2bd6ac0895c36ff93d6152
eucastillo <64089757+eucastillo@users.noreply.github.com>
2021-07-22 23:26:40 -0500

    Configuracion del sidebar
    Se corrigen detalles del sidebar

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,21 @@ var workbook = null;
 var config = null;
 
 $(function () {
-    OnLoad();
+	OnLoad();
 });
 
 $(".btnSidebar").click(function () {
-    $(this).toggleClass("click");
-    $(".sidebar").toggleClass("show");
+	$(this).toggleClass("click");
+	$(".sidebar").toggleClass("show");
+	$(".container").toggleClass("sidebarContent");
 });
 
 $(".sidebar ul li a").click(function () {
-    var id = $(this).attr("id");
-    $(".active").removeClass("active");
-    $("nav ul li ul.item-show-" + id).toggleClass("show");
-    $("nav ul li #" + id + " span").toggleClass("rotate");
-    $(this).toggleClass("active");
+	var id = $(this).attr("id");
+	$(".active").removeClass("active");
+	$("nav ul li ul.item-show-" + id).toggleClass("show");
+	$("nav ul li #" + id + " span").toggleClass("rotate");
+	$(this).toggleClass("active");
 });
 
 // $("nav ul li").click(function () {
@@ -23,135 +24,135 @@ $(".sidebar ul li a").click(function () {
 // });
 
 var OnLoad = async () => {
-    document.getElementById("applyBtn").disabled = true;
-    await readConfig();
+	document.getElementById("applyBtn").disabled = true;
+	await readConfig();
 
-    await readXls();
+	await readXls();
 
-    await initElements();
-    await fillDataTable();
+	await initElements();
+	await fillDataTable();
 
-    document.getElementById("applyBtn").disabled = false;
+	document.getElementById("applyBtn").disabled = false;
 };
 
 var loadFile = (path, responseType) => {
-    return new Promise(function (resolve, reject) {
-        let xhr = new XMLHttpRequest();
+	return new Promise(function (resolve, reject) {
+		let xhr = new XMLHttpRequest();
 
-        xhr.open("GET", path, true);
-        xhr.responseType = responseType;
+		xhr.open("GET", path, true);
+		xhr.responseType = responseType;
 
-        xhr.onload = function () {
-            if (this.status >= 200 && this.status < 300) {
-                resolve(xhr.response);
-            } else {
-                reject({
-                    status: this.status,
-                    statusText: xhr.statusText,
-                });
-            }
-        };
+		xhr.onload = function () {
+			if (this.status >= 200 && this.status < 300) {
+				resolve(xhr.response);
+			} else {
+				reject({
+					status: this.status,
+					statusText: xhr.statusText,
+				});
+			}
+		};
 
-        xhr.onerror = function () {
-            reject({
-                status: this.status,
-                statusText: xhr.statusText,
-            });
-        };
+		xhr.onerror = function () {
+			reject({
+				status: this.status,
+				statusText: xhr.statusText,
+			});
+		};
 
-        xhr.send();
-    });
+		xhr.send();
+	});
 };
 
 var readXls = async () => {
-    let xlsFile = await loadFile(config.excelPath, "arraybuffer");
+	let xlsFile = await loadFile(config.excelPath, "arraybuffer");
 
-    workbook = XLSX.read(xlsFile, { type: "buffer" });
+	workbook = XLSX.read(xlsFile, { type: "buffer" });
 };
 
 var readConfig = async () => {
-    config = await loadFile("../config.json", "json");
+	config = await loadFile("../config.json", "json");
 };
 
 var reloadFiles = async () => {
-    await readConfig();
+	await readConfig();
 
-    await readXls();
+	await readXls();
 };
 
 var initElements = async () => {
-    await initSwitch();
+	await initSwitch();
 };
 
 var initSwitch = async () => {
-    let toJson = XLSX.utils.sheet_to_json(workbook.Sheets["CatTipoPrueba"]);
+	let toJson = XLSX.utils.sheet_to_json(workbook.Sheets["CatTipoPrueba"]);
 
-    toJson.forEach((data) => {
-        let formCheck = document.createElement("div");
-        formCheck.classList.add("form-check", "form-switch", "m-2");
+	toJson.forEach((data) => {
+		let formCheck = document.createElement("div");
+		formCheck.classList.add("form-check", "form-switch", "m-2");
 
-        let inputCheck = document.createElement("input");
-        inputCheck.classList.add("form-check-input");
-        inputCheck.type = "checkbox";
-        inputCheck.checked = true;
-        inputCheck.id = data.ClaveDescripcion;
-        inputCheck.setAttribute("columnas", data.Columnas);
+		let inputCheck = document.createElement("input");
+		inputCheck.classList.add("form-check-input");
+		inputCheck.type = "checkbox";
+		inputCheck.checked = true;
+		inputCheck.id = data.ClaveDescripcion;
+		inputCheck.setAttribute("columnas", data.Columnas);
 
-        let labelCheck = document.createElement("label");
-        labelCheck.classList.add("form-check-label");
-        labelCheck.htmlFor = data.ClaveDescripcion;
-        labelCheck.textContent = data.Descripcion;
+		let labelCheck = document.createElement("label");
+		labelCheck.classList.add("form-check-label");
+		labelCheck.htmlFor = data.ClaveDescripcion;
+		labelCheck.textContent = data.Descripcion;
 
-        formCheck.appendChild(inputCheck);
-        formCheck.appendChild(labelCheck);
+		formCheck.appendChild(inputCheck);
+		formCheck.appendChild(labelCheck);
 
-        document.getElementById("switchGroup").appendChild(formCheck);
-    });
+		document.getElementById("switchGroup").appendChild(formCheck);
+	});
 };
 
 var setColumns = () => {
-    let columns = [
-        { title: "Plataforma", data: "Plataforma" },
-        { title: "Marca", data: "Marca" },
-        { title: "Talla", data: "Talla" },
-    ];
+	let columns = [
+		{ title: "Plataforma", data: "Plataforma" },
+		{ title: "Marca", data: "Marca" },
+		{ title: "Talla", data: "Talla" },
+	];
 };
 
 var fillDataTable = async () => {
-    let toJson = XLSX.utils.sheet_to_json(workbook.Sheets["TraDatosPromediados"]);
+	let toJson = XLSX.utils.sheet_to_json(workbook.Sheets["TraDatosPromediados"]);
 
-    let table = $("#resultados").DataTable({
-        //scrollX: true,
-        autoWidth: true,
-        colReorder: true,
-        searching: false,
-        ordering: false,
-        info: false,
-        paging: false,
-        // language: {
-        //     url: "dataTables.german.json",
-        // },
-        data: toJson,
-        columns: [
-            // { title: "Plataforma", data: "Plataforma" },
-            // { title: "Marca", data: "Marca" },
-            // { title: "Talla", data: "Talla" },
-            { title: "Largo Pad", data: "Largo Pad" },
-            { title: "Peso Total", data: "Peso Total" },
-            { title: "Capacidad de Absorción Total", data: "Capacidad de Absorción Total" },
-            { title: "Capacidad de Retención Total", data: "Capacidad de Retención Total" },
-            { title: "Peso del chasis", data: "Peso del chasis" },
-            { title: "Peso del chasis", data: "Peso del chasis" },
-            { title: "Peso del chasis", data: "Peso del chasis" },
-            { title: "Peso del chasis", data: "Peso del chasis" },
-            { title: "Peso del chasis", data: "Peso del chasis" },
-            { title: "Peso del chasis", data: "Peso del chasis" },
-            { title: "Peso del chasis", data: "Peso del chasis" },
+	let table = $("#resultados").DataTable({
+		//scrollX: true,
+		autoWidth: true,
+		colReorder: true,
+		searching: false,
+		ordering: false,
+		info: false,
+		paging: false,
+		// language: {
+		//     url: "dataTables.german.json",
+		// },
+		data: toJson,
+		columns: [
+			// { title: "Plataforma", data: "Plataforma" },
+			// { title: "Marca", data: "Marca" },
+			// { title: "Talla", data: "Talla" },
+			{ title: "Largo Pad", data: "Largo Pad" },
+			{ title: "Peso Total", data: "Peso Total" },
+			{ title: "Capacidad de Absorción Total", data: "Capacidad de Absorción Total" },
+			{ title: "Capacidad de Retención Total", data: "Capacidad de Retención Total" },
+			{ title: "Peso del chasis", data: "Peso del chasis" },
+			{ title: "Peso del chasis", data: "Peso del chasis" },
+			{ title: "Peso del chasis", data: "Peso del chasis" },
+			{ title: "Peso del chasis", data: "Peso del chasis" },
+			{ title: "Peso del chasis", data: "Peso del chasis" },
+			{ title: "Peso del chasis", data: "Peso del chasis" },
+			{ title: "Peso del chasis", data: "Peso del chasis" },
 
-            //{ title: "ClaveDescripcion", data: "ClaveDescripcion" },
-            //{ title: "Descripcion", data: "Descripcion" },
-        ],
-    });
+			//{ title: "ClaveDescripcion", data: "ClaveDescripcion" },
+			//{ title: "Descripcion", data: "Descripcion" },
+		],
+	});
 
-    var lalala = table.colReorder.transpose(0);
+	var lalala = table.colReorder.transpose(0);
 };
