724edbf61ac118ab656cf07e212d0e6eb7d98b58
eucastillo <64089757+eucastillo@users.noreply.github.com>
2021-07-25 16:30:38 -0500

    Acomodo de archivos

---
+++ php/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+$con = mysqli_connect("single-6020.banahosting.com","visoco27_bk","Benchmarking2021","visoco27_benchmarking");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+ }
+   echo "Connected successfully";
+
+?>
